ee4fed7d8237b20c346733355e00f67682df07cd
SaladSyrup <christopher.l.fletcher@gmail.com>
2024-02-23 21:32:08 -0800

    Periodic save

--- R/addTask-f2dbJob.R
+++ R/addTask-f2dbJob.R
@@ -5,6 +5,7 @@
 #'
 #' @param job An `f2dbJob` to add a task to.
 #' @param task An existing `f2dbTask` to add.
+#' @param followNext Recursively adds `nextTask`
 #' @inheritParams f2dbTask
 #'
 #' @returns `NULL`, invisibly.
@@ -14,18 +15,16 @@
 #' @docType methods
 #' @family f2dbJob
 #' @export
-methods::setGeneric("addTask", function(job, task, ...) standardGeneric("addTask"),
+methods::setGeneric("addTask", function(job, task, followNext, ...) standardGeneric("addTask"),
   signature = c("job", "task")
 )
 
 #-------------------------------------------------------------------------------
-#' @name addTask,f2dbJob-method
-#' @aliases addTask-method
 #' @rdname addTask-method
 #' @export
 methods::setMethod(
   "addTask", signature(job = "f2dbJob", task = "f2dbTask"),
-  function(job, task) {
+  function(job, task, followNext = FALSE) {
     stopifnot(methods::validObject(job))
     stopifnot(methods::validObject(task))
     n <- sys.parent()
@@ -41,13 +40,11 @@ methods::setMethod(
 )
 
 #-------------------------------------------------------------------------------
-#' @name addTask,list-method
-#' @aliases addTask-method
 #' @rdname addTask-method
 #' @export
 methods::setMethod(
   "addTask", signature(job = "f2dbJob", task = "list"),
-  function(job, task) {
+  function(job, task, followNext = FALSE) {
     stopifnot(methods::validObject(job))
 
     task <- as.list(task)
