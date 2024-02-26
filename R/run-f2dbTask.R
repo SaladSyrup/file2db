c3be77c785ffcf1879f0ff5d57ab10c9173d457b
SaladSyrup <christopher.l.fletcher@gmail.com>
2024-02-26 06:31:50 -0800

    Periodic save

--- R/run-f2dbTask.R
+++ R/run-f2dbTask.R
@@ -1,15 +1,40 @@
-#' @rdname f2dbRun-method
+#' f2dbRun
+#'
+#' Run an `f2dbTask`.
+#'
+#' @param object An `f2dbTask` to run.
+#' @param input Input to pass to the underlying task function.
+#' @param item The job item being processed.
+#'
+#' @returns
+#' A list of lists. The first list contains:
+#' \item{success}{Logical value indicating success (`TRUE`) or failure
+#' (`FALSE`).}
+#' \item{object}{The type of object being run.}
+#' \item{name}{The name of the object being run.}
+#' \item{messages}{Captured error, warning, or informational messages.}
+#'
+#' Subsequent lists contain the output from running `nextTask`.
+#'
+#' @name f2dbRun,f2dbTask-method
+#' @docType methods
+#' @family f2dbTask
+#' @family f2dbRun methods
 #' @export
 methods::setMethod(
   "f2dbRun", "f2dbTask",
   function(object, input = NA, item = NA) {
     functionOutput <- f2dbRun(taskFunction(object), input, item)
+    retValue <- list(
+      success = functionOutput$success, object = class(object)[1],
+      name = name(object), messages = functionOutput$messages
+    )
 
     if (functionOutput$success == FALSE) {
-      return(list(taskName = name(object), success = FALSE, functionOutput = functionOutput$output))
+      return(retValue)
     }
 
-    taskOutput <- f2dbRun(nextTask(object), functionOutput$output, item)
-    list(list(taskName = name(object), success = TRUE, functionOutput = functionOutput$output), taskOutput)
+    retNextTask <- f2dbRun(nextTask(object), functionOutput$output, item)
+    c(retValue, retNextTask)
   }
 )
