821c65a0d361c224ad7025144b584b97fd0686cb
SaladSyrup <christopher.l.fletcher@gmail.com>
2024-03-05 22:17:02 -0800

    Periodic save.

---
+++ R/f2dbAppendTable.R
@@ -0,0 +1,21 @@
+#' f2dbAppendTable
+#'
+#' A `f2dbTask` for appending data to a database table.
+#'
+#' @name f2dbAppendTable-class
+#' @docType class
+#' @family f2dbTask
+#' @family f2db classes
+#' @export
+methods::setClass("f2dbAppendTable", contains = c("f2dbTask"))
+
+#-------------------------------------------------------------------------------
+#' f2dbAppendTable
+#'
+#' `f2dbAppendTable` constructor.
+#'
+#' @returns An `f2dbAppendTable` object.
+#'
+#' @family f2dbTask
+#' @export
+f2dbAppendTable <- function(){}
